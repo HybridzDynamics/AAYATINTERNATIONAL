680ac6b48b327bb88cbcc1909d1662d6e9809df6
HybridzDynamics <hybridgamming1655@gmail.com>
2024-12-28 22:02:46 +0530

    Initial commit

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
-console.log('Website Loaded');
-alert('Welcome to AAYAT INTERNATIONAL!');
+// Smooth scroll functionality for the "Learn More" button
+document.querySelector('button').addEventListener('click', function () {
+  document.querySelector('#about').scrollIntoView({ behavior: 'smooth' });
+});
